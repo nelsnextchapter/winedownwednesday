a1983688e672fc6a9645bd8e4f4b85c065085ef4
nelsnextchapter <neldiamond17@gmail.com>
2025-12-01 19:41:40 -0500

    green christmas spinner wheel.js
    i accidently copied the wrong color code last time so it was red. this is the correct one which should be green

--- script.js
+++ script.js
@@ -681,8 +681,8 @@ function drawWheel() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   function getColorForIndex(index, total) {
-  const baseHue = 0;           // HSL for #CB550D
-  const baseSaturation = 87;    // %
+  const baseHue = 94;           // HSL for #CB550D
+  const baseSaturation = 98;    // %
   const baseLightness = 40;     // %
 
   // Adjust lightness slightly for variation
